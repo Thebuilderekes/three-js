4e808cae6bc70ad708d31dddeb337fde36a54a88
thebuilderekes <ekeopreberedugo1@gmail.com>
2023-06-18 10:36:04 +0100

    adied mesh and added scene to mesh

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 //Scene
 const scene = new THREE.Scene();
 
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-	color: "00ff83",
+	color: "#ffffff",
 });
 
 //to mesh ecerything together
 const mesh = new THREE.Mesh(geometry, material);
 
-//k
+// to put the mesh into the scene
+
 scene.add(mesh);
